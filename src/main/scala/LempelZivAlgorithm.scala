6f18e8b7784609926bfabbc0ec8c0be04d03d9d2
Jaime Guzman <jguzman@ratslabs.io>
2015-11-20 18:12:59 -0300

    Update Engine and Model ALgo

--- src/main/scala/LempelZivAlgorithm.scala
+++ src/main/scala/LempelZivAlgorithm.scala
@@ -9,10 +9,9 @@ import io.prediction.controller.Params
 import io.prediction.data.storage.BiMap
 
 import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
 
 import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.mllib.tree.DecisionTree
+
 import org.apache.spark.mllib.tree.model.DecisionTreeModel
 import org.apache.spark.mllib.tree.configuration.Strategy
 import org.apache.spark.mllib.tree.configuration.Algo
@@ -27,8 +26,12 @@ import org.apache.spark.mllib.tree.configuration.Algo._
 import grizzled.slf4j.Logger
 
 
-case class LZModel(
+class LZModel(
         val valor: Int
+
+
+
+
         ) extends Serializable {
 
 //  @transient lazy val itemIntStringMap = itemStringIntMap.inverse
@@ -40,36 +43,34 @@ case class LZModel(
 
 
 case class AlgorithmParams(
-                            numAlphabet: Integer
-                            ) extends Params
 
-// extends P2LAlgorithm because the MLlib's NaiveBayesModel doesn't contain RDD.
-
+  numAlphabet: Integer
+) extends Params
 
 
 
 
 
-class Algorithm(val ap: AlgorithmParams) extends P2LAlgorithm[PreparedData, LZModel, Query, PredictedResult] {
+
+
+abstract class Algorithm(val ap: AlgorithmParams)
+  extends P2LAlgorithm[ PreparedData,
+                        Any,
+                        Query,
+                        PredictedResult] {
 
   @transient lazy val logger = Logger[this.type]
 
   def train(sc: SparkContext ,   data: PreparedData): Any = {
 
 
-    // MLLib DecisionTree cannot handle empty training data.
-    require(!data.labeledPoints.take(1).isEmpty,
-      s"RDD[labeldPoints] in PreparedData cannot be empty." +
-        " Please check if DataSource generates TrainingData" +
-        " and Preprator generates PreparedData correctly.")
+    logger.debug( "::::::  en TRAIN  ::::" )
 
-    var m=Map[Integer,Integer]()
-    var categoricalFeaturesInfo: java.util.Map[Integer,Integer] = mapAsJavaMap[Integer, Integer](m)
-    val impurity = "gini"
 
-    //val stat= new Strategy(algo = Classification, impurity = Gini, ap.maxDepth, ap.numClasses,ap.maxBins, categoricalFeaturesInfo)
-    //val tree=new DecisionTree(stat)
-    //tree.run(data.labeledPoints)
+
+    System.out.println("Hola ")
+
+
 
 
 
@@ -80,10 +81,19 @@ class Algorithm(val ap: AlgorithmParams) extends P2LAlgorithm[PreparedData, LZMo
 
 
 
+  /**El predictor es la lectura del TRIE LZ ***/
+  def predict(model: Any, query: Query): PredictedResult = {
 
-  def predict(model: DecisionTreeModel, query: Query): PredictedResult = {
-    val label = model.predict(Vectors.dense(query.features))
+    System.out.println("Entro al predictor" )
+
+
+
+
+
+    val label =  6.9 //"esto es un etiqueta " //model.predict(Vectors.dense(query.features))
     new PredictedResult(label)
+
+
 
   }
 
---
+++ src/main/scala/Trie.scala
@@ -0,0 +1,6 @@
+/**
+ * Created by jguzman on 20-11-15.
+ */
+class Trie {
+
+}
